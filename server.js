87cf1037ac53deb1883a9888a8aaef4370f5500d
Joseph DeChicchis <josephdechicchis@gmail.com>
2016-12-10 16:43:53 -0500

    added twitter stream. tmeplate code did not work

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
 // Require our dependencies
-var express = require('express'),
-  exphbs = require('express-handlebars'),
-  http = require('http'),
-  mongoose = require('mongoose'),
-  twitter = require('twitter'),
-  routes = require('./routes'),
-  config = require('./config'),
-  streamHandler = require('./utils/streamHandler');
+var express = require('express');
+
+  var Twitter = require('node-tweet-stream')
+    , t = new Twitter({
+      consumer_key: '6kpisyD2fy1unFRWXmsvgkqjU',
+      consumer_secret: 'qkXATKioAgkIPsXI8AnP84u7VrZDipDjCNkFk5oyWrL6Om7QSx',
+      token: '1492647266-4MgvXfRNVzx6l8CHRCjNxkiKGR94XtgXI41rfa0',
+      token_secret: 'MupsKT27Ga9zU0lRWKu1INHHpBCGbksCI6gFLR9gu7DKk'
+    })
 
 // Create an express instance and set a port variable
 var app = express();
-var port = process.env.PORT || 8080;
-
-// Set handlebars as the templating engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// Disable etag headers on responses
-app.disable('etag');
-
-// Connect to our mongo database
-mongoose.connect('mongodb://localhost/react-tweets');
-
-// Create a new ntwitter instance
-var twit = new twitter(config.twitter);
-
-// Index Route
-app.get('/', routes.index);
-
-// Page Route
-app.get('/page/:page/:skip', routes.page);
+app.set('port', (process.env.PORT || 5000));
 
 // Set /public as our static content dir
 app.use("/", express.static(__dirname + "/public/"));
 
-// Fire this bitch up (start our server)
-var server = http.createServer(app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'));
 });
 
+t.on('tweet', function (tweet) {
+  console.log('tweet received', tweet)
+})
+
+t.on('error', function (err) {
+  console.log('Oh no')
+})
+
+t.track('refugee');
+t.track('');
+
+
+/*
 // Initialize socket.io
 var io = require('socket.io').listen(server);
 
 // Set a stream listener for tweets matching tracking keywords
+
+console.log("BREAK 1");
+
 twit.stream('statuses/filter',{ track: 'javascript'}, function(stream){
+  console.log("BREAK 2");
   streamHandler(stream,io);
 });
+
+function streamHandler(stream,io) {
+
+  console.log("ININ");
+
+}*/
